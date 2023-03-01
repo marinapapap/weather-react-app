d1c107350bcae2054d5c3053db82a778af0b8e97
Marina Papapetrou <contact@marinapapap.com>
2023-03-01 19:29:50 +0000

    /weather route in server returns weather data - city is currently hardcoded in as frontend not yet implemented.

--- server/controllers/weather.js
+++ server/controllers/weather.js
@@ -1,9 +1,18 @@
- const WeatherController = {
+const WeatherController = {
     GetWeather: async (req, res) => {
-        res.status(200).json({
-            message: "route test"
-        })
+        const city = "paris";
+
+        return fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${process.env.API_KEY}`).then((response) => {
+            return response.json();
+        }).then((responseData) => {
+            res.status(200).json({
+                message: "ok",
+                results: responseData
+            })
+        }).catch((error) => {
+            console.error(error);
+            });
     }
- }
+}
 
  module.exports = WeatherController;
