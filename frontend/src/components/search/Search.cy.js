f3b428212671bd2854089d08c9d1a4e67dfb37b2
Marina Papapetrou <contact@marinapapap.com>
2023-03-06 16:10:43 +0000

    Updated test checking for location-search field to also check for submit-location submit button.

--- frontend/src/components/search/Search.cy.js
+++ frontend/src/components/search/Search.cy.js
@@ -1,11 +1,11 @@
 import Search from "./Search";
 
 describe("Search", () => {
-    it("has location text input field", () => {
+    it("has location text input field and submit button", () => {
         cy.mount(<Search/>);
-        
-        cy.get('[data-cy="location-search"]')
+        cy.get('[data-cy="location-search"]');
+        cy.get('[data-cy="submit-location"]')
             .invoke("attr", "type")
-            .should("eq", "text");
-      });
+            .should("eq", "submit");
+    });
 })
