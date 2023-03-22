63f2b35db641f68627f79178693289717ce4f8b4
Marina Papapetrou <contact@marinapapap.com>
2023-03-22 21:54:34 +0000

    Wrote and passed tests to check for a 400 response code if no location is given or the provided location isn't found.

--- server/spec/controllers/weather.spec.js
+++ server/spec/controllers/weather.spec.js
@@ -3,29 +3,57 @@ const request = require("supertest");
 require("jest-fetch-mock").enableMocks();
 
 describe("/weather", () => {
-  let response;
-  beforeEach(async () => {
-    fetch.resetMocks();
+  describe("Good request made", () => {
+    let response;
 
-    fetch.mockResponseOnce(
-      JSON.stringify({
-        cod: 200,
-        weather: "cloudy",
-      })
-    );
+    beforeEach(async () => {
+      fetch.resetMocks();
+      fetch.mockResponseOnce(
+        JSON.stringify({
+          cod: 200,
+          weather: "cloudy",
+        })
+      );
+      response = await request(app).get("/weather?location=london");
+    });
 
-    response = await request(app).get("/weather?location=london");
+    test("Response code is 200 when given location param", async () => {
+      expect(response.status).toEqual(200);
+      expect(response.body.message).toEqual("ok");
+    });
+
+    test("calls fetch for london weather", async () => {
+      expect(fetch).toHaveBeenCalledWith(
+        expect.stringContaining(
+          "http://api.openweathermap.org/data/2.5/weather?units=metric&q=london&appid="
+        )
+      );
+    });
   });
 
-  test("Response code is 200 when given location param", async () => {
-    expect(response.status).toEqual(200);
-  });
+  describe("Bad request made", () => {
+    beforeEach(async () => {
+      fetch.resetMocks();
+    });
 
-  test("calls fetch for london weather", async () => {
-    expect(fetch).toHaveBeenCalledWith(
-      expect.stringContaining(
-        "http://api.openweathermap.org/data/2.5/weather?units=metric&q=london&appid="
-      )
-    );
+    test("Response code is 400 when no location is given", async () => {
+      const response = await request(app).get("/weather?location=");
+
+      expect(response.status).toEqual(400);
+      expect(response.body.message).toEqual("No location provided.");
+    });
+
+    test("Response code is 400 if location is not found", async () => {
+      fetch.mockResponseOnce(
+        JSON.stringify({
+          cod: 400,
+          message: "city not found",
+        })
+      );
+      const response = await request(app).get("/weather?location=notacity");
+
+      expect(response.status).toEqual(400);
+      expect(response.body.message).toEqual("city not found");
+    });
   });
 });
